f97355ed8b23b2741546a84ad59f1cf4f03ee25e
Pirlolo <1826097860@qq.com>
2020-12-24 10:20:34 +0800

    完成用户列表的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    redirect: '/welcome',
+    component: Home, children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -22,12 +31,12 @@ router.beforeEach((to, from, next) => {
   //next 是一个函数，表示放行
   //    next()  放行   next('/login')  强制跳转
 
-  if(to.path === '/login'){
+  if (to.path === '/login') {
     return next();
   }
   //获取 token
   const tokenStr = window.sessionStorage.getItem("token");
-  if(!tokenStr){
+  if (!tokenStr) {
     return next('/login');
   }
   next();
